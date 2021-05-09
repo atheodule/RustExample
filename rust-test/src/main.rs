69d436d912a0c4849b1a94afbe3df3d9ec9ae54d
[atheodule] <akt30@pitt.edu>
2021-05-09 03:22:25 -0400

    First commit
    rust hello world example

---
+++ rust-test/src/main.rs
@@ -0,0 +1,23 @@
+//! Prints "Hello, world!" on the host console using semihosting
+
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+
+#[entry]
+fn main() -> ! {
+
+    hprintln!("").unwrap();
+    hprintln!("Hello  world!").unwrap();
+    hprintln!("Crusty Beast Tamed!!").unwrap();
+
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
+}
